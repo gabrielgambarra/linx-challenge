8c38d79b4c335ba85b207a2d912f06a92f30e053
Gabriel Gambarra <gabriel.gambarra@itriad.org.br>
2019-11-25 16:39:56 -0400

    fix structure and imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppComponent } from './app.component';
-import { MaterialModule } from './shared/material-module/material.module';
 import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 import { ROUTES } from './app.routes';
-import { MainComponent } from './main/main.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from './shared/material-module/material.module';
+import { MainComponent } from './web/main/main/main.component';
 
 
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { MainComponent } from './main/main.component';
+import { MainComponent } from './web/main/main/main.component';
 
 export const ROUTES: Routes = [
   { path: '', component: MainComponent }
