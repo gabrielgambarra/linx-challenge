2f87dd57401202644659526b441fc0718e793a01
Gabriel Gambarra <gabriel.gambarra@itriad.org.br>
2019-11-25 17:45:35 -0400

    Apply responsiveness of header and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'linx-challenge';
+export class AppComponent implements OnInit {
+
+  isMobile: boolean = false;
+  
+  ngOnInit(): void {
+    // Check if screen mobile to apply responsiveness
+    this.isMobile = window.innerWidth < 700 ? true : false;
+  }
+
 }
